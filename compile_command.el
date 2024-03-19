61ae9989ec475eec045c6f9eb633abc0447a61af
Chris Hipple <chrishipple@multimediallc.com>
2024-03-19 16:50:40 -0400

    add some movement commands, cleanup compile-command.el

--- compile_command.el
+++ compile_command.el
@@ -1,32 +1,17 @@
-(defun compile-def-to-paren-with-docker ()
+(defun run-individual-pytest ()
   "Compile using 'docker compose run --rm test <FULL_PATH>::'
    followed by the text from after 'def' up to but not including the first '(' on the current line."
   (interactive)
   (save-excursion
-    (message "starting")
     (setq my_line (thing-at-point 'line))
-    (message my_line)
 
     (let ((pattern "^def \\([a-zA-Z0-9_]+\\)"))
       (when (string-match pattern my_line)
         (setq function-name (match-string 1 my_line))))
 
-    (message function-name)
-
-    (setq original-string "Hello, world! Hello, Emacs!")
-    (setq string-to-replace "Hello")
-    (setq replacement-string "Goodbye")
-
-    (setq new-string (replace-regexp-in-string string-to-replace replacement-string original-string))
-
     (setq fullpath (buffer-file-name))
-    (message fullpath)
     (setq root (projectile-project-root))
-    (message root)
     (setq final_path (replace-regexp-in-string root "" fullpath))
-    (message final_path)
-
-    (message "about to concat")
     (setq command (concat "docker compose run --rm test "
                           final_path
                           "::"
@@ -34,9 +19,28 @@
                           " --no-cov --reuse-db --no-migrations"
                           ))
     (message command)
+    (compile command))
+  )
+
+
+(define-key evil-normal-state-map (kbd "SPC c t") 'run-individual-pytest)
+
+(defun run-pytest-full-file ()
+  "Compile using 'docker compose run --rm test <FULL_PATH>::'
+   followed by the text from after 'def' up to but not including the first '(' on the current line."
+  (interactive)
+  (save-excursion
+    (setq fullpath (buffer-file-name))
+    (setq root (projectile-project-root))
+    (setq final_path (replace-regexp-in-string root "" fullpath))
+
+    (setq command (concat "docker compose run --rm test "
+                          final_path
+                          " --no-cov --reuse-db --no-migrations"
+                          ))
     (compile command)))
 
-(define-key evil-normal-state-map (kbd "SPC c t") 'compile-def-to-paren-with-docker)
+(define-key evil-normal-state-map (kbd "SPC c T") 'run-pytest-full-file)
 
 
   (defun cb-lint ()
