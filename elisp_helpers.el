d9f9f0fa0645b96a4d10309b1b9a491f29de0d69
Chris Hipple <chrishipple@multimediallc.com>
2024-05-29 10:31:27 -0400

    make org always use fixed-width font regardles of theme

--- elisp_helpers.el
+++ elisp_helpers.el
@@ -25,3 +25,27 @@
 
 (define-key evil-normal-state-map (kbd ", l b") 'send-buffer-to-llm)
 
+
+(defun my/org-mode-use-fixed-width-font ()
+  "Set Org mode faces to use a fixed-width font."
+  (interactive)
+  (set-face-attribute 'org-table nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-block nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-document-info-keyword nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-meta-line nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-document-info nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-verbatim nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-special-keyword nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-property-value nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-column nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-column-title nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-drawer nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-level-1 nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-level-2 nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-level-3 nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-level-4 nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-level-5 nil :inherit 'fixed-pitch)
+  )
+
+(set-face-attribute 'fixed-pitch nil :family "Courier New")
