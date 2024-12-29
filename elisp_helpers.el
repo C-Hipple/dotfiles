987c2fd21ec6a6ef02ac7ec30fa05416896af921
Chris <hipplec@Chriss-MacBook-Pro.local>
2024-12-29 13:04:04 -0500

    personal changes

--- elisp_helpers.el
+++ elisp_helpers.el
@@ -74,3 +74,25 @@
   )
 
 (add-hook 'org-mode-hook 'my/org-mode-use-fixed-width-font)
+
+
+;;can get versioned list here
+;; https://github.com/mickeynp/combobulate/blob/ca2545be196ec32945b5db109578582d7c7270b5/.ts-setup.el#L2
+
+;; from https://www.masteringemacs.org/article/how-to-get-started-tree-sitter
+(setq treesit-language-source-alist
+      '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+        (cmake "https://github.com/uyha/tree-sitter-cmake")
+        (css "https://github.com/tree-sitter/tree-sitter-css")
+        (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+        (go "https://github.com/tree-sitter/tree-sitter-go")
+        (html "https://github.com/tree-sitter/tree-sitter-html")
+        (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+        (json "https://github.com/tree-sitter/tree-sitter-json")
+        (make "https://github.com/alemuller/tree-sitter-make")
+        (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+        (python "https://github.com/tree-sitter/tree-sitter-python")
+        (toml "https://github.com/tree-sitter/tree-sitter-toml")
+        (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+        (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+        (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
--- zshrc
+++ zshrc
@@ -2,5 +2,6 @@
 alias python=/usr/local/bin/python3.10
 alias pip=/usr/local/bin/pip3.10
 
-PS1="%1~ : "
+export PATH=${PATH}:`go env GOPATH`/bin
 
+PS1="%1~: "
